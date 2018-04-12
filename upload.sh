058ab165d7369a7ca60264365163669e5d2b8919
Viktoras Makauskas <viktoras@neglostyti.com>
2018-04-12 23:35:15 +0300

    fixing dependency on external css

---
+++ upload.sh
@@ -0,0 +1,10 @@
+rm -rf public || exit 2
+~/apps/hugo/hugo.exe || exit 1
+
+venv/Scripts/activate
+pip install awscli || exit 3
+
+echo "syncing with AWS..."
+aws s3 sync public s3://neglostyti.com --acl public-read
+
+echo "Done."
